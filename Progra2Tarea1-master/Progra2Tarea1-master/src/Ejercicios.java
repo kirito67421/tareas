2af770a78680a31dfc02367c87a97e7165cba9ae
Kevin Mendez <gerardo_soft@unitec.edu>
2015-02-06 20:34:03 -0600

    Tarea1
    Tarea1

---
+++ Progra2Tarea1-master/Progra2Tarea1-master/src/Ejercicios.java
@@ -0,0 +1,100 @@
+public class Ejercicios
+{
+	//deveulve true si "numero" es par y false de lo contrario
+	static boolean esPar(int numero)
+	{
+		if(numero%2==1) 
+			return false;
+		else
+			return true;	
+	}
+	
+	
+	//devuelve "gratis" si lempiras es igual a 0
+	//devuelve "barato" si lempiras es menor que 100
+	//devuelve "normal" si lempiras es menor que 200
+	//de lo contrario devuelve caro
+	//nota: solo aceptan minusculas
+	static String obtenerPrecio(int lempiras)
+	{
+		if(lempiras==0)
+		return "gratis";
+		if(lempiras>0 & lempiras<100)
+		return "barato";
+		if(lempiras>100 &lempiras<200)
+		return "normal";
+		if(lempiras>200)
+		return "caro";
+		return null;
+	}
+	
+	//devuelve el factorial de "num"
+	static int factorial(int num)
+	{ int res=1;
+	 
+	for(int i=num;i>0;i--)
+	 {res=i*res;}
+	return res;
+	}
+
+	
+	//devuelve el elemento de "arr" en la posicion "pos"
+	static int getElemento(int arr[],int pos)
+	{   int resu=0;
+		resu=arr[pos];
+		return resu;
+	}
+	
+	//devuelve el elemento de "arr" en la posicion ["col"] ["fila"]
+	static int getElemento(int arr[][],int col,int fila)
+	{ int resu=0;
+	resu=arr[col][fila];
+		return resu;
+	}
+	
+	//devuelve el atributo "x" de "MiClase"
+	static int getX(MiClase mi_clase)
+	{ 
+		return mi_clase.x;
+	}
+	
+	//implementar la funcion "getXPor2()" descrita en la clase "MiClase"
+	//No modificar esta funcion
+	static int implementarGetXPor2(MiClase mi_clase)
+	{
+		return mi_clase.getXPor2();
+	}
+	
+	//implementar la funcion "getXPor2()" descrita en la clase "MiClase"
+	//No modificar esta funcion
+	static int implementarGetXMasY(MiClase mi_clase, int y)
+	{
+		return mi_clase.getXMasY(y);
+	}
+	
+	//devuelve la funcion fibonacci de "num"
+	//nota: puedes consultar la funcion en http://es.wikipedia.org/wiki/Sucesi%C3%B3n_de_Fibonacci
+	static int fibonacci(int num)
+	{
+		if(num==1) return 1;
+		else if (num==0) return 0;
+		return fibonacci(num-1) + fibonacci(num-2);
+	}
+	
+	//realizar la siguiente funcion recursiva:
+	//funcionRecursiva(n) = funcionRecursiva(n-1)*2+1
+	//donde: funcionRecursiva(0) = 1 y funcionRecursiva(1) = 2
+	static int funcionRecursiva(int num)
+	{
+		if(num==0) return 1;
+		else if(num==1) return 2;
+		return funcionRecursiva(num-1)*2+1;
+	}
+	
+	public static void main(String[] args)
+	{
+
+	}
+
+}
+
